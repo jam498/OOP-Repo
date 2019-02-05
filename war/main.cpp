6291788141ed106a46ac0ef046bf61b97e8ad969
jam498 <46718498+jam498@users.noreply.github.com>
2019-02-05 04:40:18 -0500

    Rename War/main.cpp to war/main.cpp

--- war/main.cpp
+++ war/main.cpp
@@ -33,6 +33,58 @@ struct Card
 	Suit suit;
 	Rank rank;
 };
+std::ostream& operator<<(std::ostream& os, Rank r) {
+	switch (r)
+	{
+	case(Ace) : os << "Ace";
+		break;
+	case(Two) : os << "Two";
+		break;
+	case(Three) : os << "Three";
+		break;
+	case(Four) : os << "Four";
+		break;
+	case(Five) : os << "Five";
+		break;
+	case(Six) : os << "Six";
+		break;
+	case(Seven) : os << "Seven";
+		break;
+	case(Eight) : os << "Eight";
+		break;
+	case(Nine) : os << "Nine";
+		break;
+	case(Ten) : os << "Ten";
+		break;
+	case(Jack) : os << "Jack";
+		break;
+	case(Queen) : os << "Queen";
+		break;
+	case(King) : os << "King";
+		break;
+	}
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, Suit s) {
+	switch (s)
+	{
+	case(Spades) : os << "Spades";
+		break;
+	case(Clubs) : os << "Clubs";
+		break;
+	case(Hearts) : os << "Hearts";
+		break;
+	case(Diamonds) : os << "Diamonds";
+		break;
+	}
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, Card c) {
+	os << c.rank << " of " << c.suit;
+	return os;
+}
 
 void buildDecks(stack<Card>&, stack<Card>&);
 void war(stack<Card>, stack<Card>);
@@ -65,7 +117,7 @@ void buildDecks(stack<Card>& p1, stack<Card>&p2)
 
 	for (int k = 52; k > 1; k--)
 	{
-		divide = rand() % (k-1);
+		divide = rand() % (k - 1);
 		if (k % 2 == 0)
 			p1.push(deck[divide]);
 		else
@@ -83,12 +135,12 @@ void war(stack<Card> p1, stack<Card>p2)
 		if (static_cast<int>(p1.top().rank) > static_cast<int>(p2.top().rank))
 		{
 			player1++;
-			std::cout << "Player 1 takes the round\n";
+			std::cout << p1.top() << "beats " << p2.top() << ", Point to Player 1\n";
 		}
 		else if (static_cast<int>(p1.top().rank) < static_cast<int>(p2.top().rank))
 		{
 			player2++;
-			std::cout << "Player 2 takes the round\n";
+			std::cout << p2.top() << " beats " << p1.top() << ", Point to Player 2\n";
 		}
 		p1.pop();
 		p2.pop();
